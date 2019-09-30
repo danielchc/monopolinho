ee3af1273dba4496f2ed3b6db953e4b2cf59dc0c
Daniel <danielchenelcea@gmail.com>
2019-09-30 11:30:46 +0200

    Commit

--- src/main/java/monopolinho/Casilla.java
+++ src/main/java/monopolinho/Casilla.java
@@ -1,165 +1,30 @@
 package monopolinho;
 
 
+import java.util.ArrayList;
+
 public class Casilla {
-    enum TipoCasilla{
-        CARCEL,
-        SOLAR,
-        PUTAMIERDA
-    }
-    /* Atributos */
-    private String NombreCasilla;
-    private TipoCasilla TipoCasilla;
-    private String GrupoCasilla; //Color da casilla
-    private Xogador Propietario;
-    private float Valor;
-    private float Alquiler;
-    private float ValorHotel;
-    private float ValorCasa;
-    private float ValorPiscina;
-    private float ValorPistaDeporte;
-    private float AlquilerUnhaCasa;
-    private float AlquilerDuasCasas;
-    private float AlquilerTresCasas;
-    private float AlquilerCatroCasas;
-    private float AlquilerHotel;
-    private float AlquilerPiscina;
-    private float AlquilerPistaDeporte;
-
-    /* Constructores */
-    public Casilla(String NombreCasilla,TipoCasilla TipoCasilla){
-        this.NombreCasilla=NombreCasilla;
-        this.TipoCasilla=TipoCasilla;
+    private String nome;
+    private String tipo;
+    private float valor;
+    private int posicion;
+    private Xogador dono;
+    public Casilla(String nome,String tipo,int posicion, float valor){
+        this.nome=nome;
+        this.tipo=tipo;
+        this.posicion=posicion;
+        this.valor=valor;
     }
 
-    /* Getters e setters */
-    public String getNombreCasilla(){
-        return NombreCasilla;
-    }
-    public void setNombreCasilla(String NombreCasilla){
-        this.NombreCasilla=NombreCasilla;
+    public String getNome() {
+        return nome;
     }
 
-    public TipoCasilla getTipoCasilla(){
-        return TipoCasilla;
-    }
-    public void setTipoCasilla(TipoCasilla TipoCasilla){
-        this.TipoCasilla=TipoCasilla;
+    public Xogador getDono() {
+        return dono;
     }
 
-    public String getGrupoCasilla(){
-        return GrupoCasilla;
-    }
-    public void setGrupoCasilla(String GrupoCasilla){
-        this.GrupoCasilla=GrupoCasilla;
-    }
-
-    public Xogador getPropietario(){
-        return Propietario;
-    }
-    public void setPropietario(Xogador Propietario){
-        this.Propietario=Propietario;
-    }
-
-    public float getValor(){
-        return Valor;
-    }
-    public void setValor(float Valor){
-        this.Valor=Valor;
-    }
-
-    public float getAlquiler(){
-        return Alquiler;
-    }
-    public void setAlquiler(float Alquiler){
-        this.Alquiler=Alquiler;
-    }
-
-    public float getValorHotel(){
-        return ValorHotel;
-    }
-    public void setValorHotel(float ValorHotel){
-        this.ValorHotel=ValorHotel;
-    }
-
-    public float getValorCasa(){
-        return ValorCasa;
-    }
-    public void setValorCasa(float ValorCasa){
-        this.ValorCasa=ValorCasa;
-    }
-
-    public float getValorPiscina(){
-        return ValorPiscina;
-    }
-    public void setValorPiscina(float ValorPiscina){
-        this.ValorPiscina=ValorPiscina;
-    }
-
-    public float getValorPistaDeporte(){
-        return ValorPistaDeporte;
-    }
-    public void setValorPistaDeporte(float ValorPistaDeporte){
-        this.ValorPistaDeporte=ValorPistaDeporte;
-    }
-
-    public float getAlquilerUnhaCasa(){
-        return AlquilerUnhaCasa;
-    }
-    public void setAlquilerUnhaCasa(float AlquilerUnhaCasa){
-        this.AlquilerUnhaCasa=AlquilerUnhaCasa;
-    }
-
-    public float getAlquilerDuasCasas(){
-        return AlquilerDuasCasas;
-    }
-    public void setAlquilerDuasCasas(float AlquilerDuasCasas){
-        this.AlquilerDuasCasas=AlquilerDuasCasas;
-    }
-
-    public float getAlquilerTresCasas(){
-        return AlquilerTresCasas;
-    }
-    public void setAlquilerTresCasas(float AlquilerTresCasas){
-        this.AlquilerTresCasas=AlquilerTresCasas;
-    }
-
-    public float getAlquilerCatroCasas(){
-        return AlquilerCatroCasas;
-    }
-    public void setAlquilerCatroCasas(float AlquilerCatroCasas){
-        this.AlquilerCatroCasas=AlquilerCatroCasas;
-    }
-
-    public float getAlquilerHotel(){
-        return AlquilerHotel;
-    }
-    public void setAlquilerHotel(float AlquilerHotel){
-        this.AlquilerHotel=AlquilerHotel;
-    }
-
-    public float getAlquilerPiscina(){
-        return AlquilerPiscina;
-    }
-    public void setAlquilerPiscina(float AlquilerPiscina){
-        this.AlquilerPiscina=AlquilerPiscina;
-    }
-
-    public float getAlquilerPistaDeporte(){
-        return AlquilerPistaDeporte;
-    }
-    public void setAlquilerPistaDeporte(float AlquilerPistaDeporte){
-        this.AlquilerPistaDeporte=AlquilerPistaDeporte;
-    }
-
-    /* Para imprimir a información da casilla */
-    @Override
-    public String toString(){
-        String informacion="\nTipo: "+this.TipoCasilla+"\nGrupo: "+this.GrupoCasilla+"\nPropietario: "+this.Propietario
-                            +"\nValor: "+this.Valor+"\nAlquiler: "+this.Alquiler+"\nValor Casa: "+this.ValorCasa
-                            +"\nValor Hotel: "+this.ValorHotel+"\nValor Piscina: "+this.ValorPiscina+"\nValor Pista de Deporte: "+this.ValorPistaDeporte
-                            +"\nAlquiler 1 Casa: "+this.AlquilerUnhaCasa+"\nAlquiler 2 Casas: "+this.AlquilerDuasCasas+"\nAlquiler 3 Casas: "+this.AlquilerTresCasas+"\nAlquiler 4 Casas: "+this.AlquilerCatroCasas
-                            +"\nAlquiler Hotel: "+this.AlquilerHotel+"\nAlquiler Piscina: "+this.AlquilerPiscina+"\nAlquiler Pista de Deporte: "+this.AlquilerPistaDeporte;
-        return informacion;
+    public void setDono(Xogador dono) {
+        if(dono!=null)this.dono = dono;
     }
 }
--- src/main/java/monopolinho/Xogador.java
+++ src/main/java/monopolinho/Xogador.java
@@ -1,25 +1,52 @@
 package monopolinho;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Xogador {
     private String nome;
     private Avatar avatar;
     private float fortuna;
     private float dineroGastado;
+    private ArrayList<Casilla> propiedades;
 
     public Xogador(){
         this.nome="Banca";
         this.avatar=null;
         this.dineroGastado=0;
         this.fortuna=Integer.MAX_VALUE; //1000000000
+        this.propiedades=new ArrayList<>(); //lookoooo
     }
 
     public Xogador(String nome, Avatar.TipoAvatar tipoAvatar){
         this.nome=nome;
-        this.fortuna=Valores.FORTUNA_INCIAL;
+        this.fortuna=100000;
         this.dineroGastado=0;
         this.avatar=new Avatar(tipoAvatar,this);
+        this.propiedades=new ArrayList<>(); //lookoooo
+
     }
 
+    public ArrayList<Casilla> getPropiedades() {
+        return propiedades;
+    }
+
+    public void setPropiedades(ArrayList<Casilla> propiedades) {
+        if(propiedades==null){
+            System.err.println("Propiedades non inicializada");
+            return;
+        }
+        for(Casilla casilla:propiedades){
+            if (casilla==null){
+                System.err.println("Casilla non inicializada");
+                return;
+            }
+        }
+        this.propiedades=propiedades;
+    }
+    public void engadirPropiedade(Casilla casilla){
+        if(casilla!=null)this.propiedades.add(casilla);
+    }
     public void setNome(String nome) {
         if(nome!=null)this.nome = nome;
     }
@@ -47,6 +74,16 @@ public class Xogador {
 
     @Override
     public String toString(){
-        return String.format("{\n\tnombre:%s,\n\tavatar:%s\n}",getNome(),getAvatar().getId());
+        String listaprop="[";
+        if(this.propiedades.size()!=0)
+            for(Casilla c:this.propiedades)
+                listaprop+="\n\t\t"+c.getNome()+",";
+        listaprop+="\n\t]";
+        return "{\n\tNome:" +this.nome+ ",\n"+
+                "\tAvatar:"+ ((getAvatar()!=null)?getAvatar().getId():"NON TEN")+ ",\n"+
+                "\tFortuna:"+ this.fortuna+ ",\n"+
+                "\tGastos:"+  this.dineroGastado +",\n"+
+                "\tPropiedades:"+listaprop+"\n"+
+                "}";
     }
 }
