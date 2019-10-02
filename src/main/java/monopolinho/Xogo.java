e3019643c075bb3b46aed2387d01e3f3eb2ed7d9
David <davidcm62@gmail.com>
2019-10-02 12:43:24 +0200

    Commit

--- src/main/java/monopolinho/Xogo.java
+++ src/main/java/monopolinho/Xogo.java
@@ -20,7 +20,7 @@ public class Xogo {
     public void consola(){
         Scanner scanner= new Scanner(System.in);
         while(true){
-            System.out.println("$> ");
+            System.out.print("$> ");
             interpretarComando(scanner.nextLine());
         }
     }
