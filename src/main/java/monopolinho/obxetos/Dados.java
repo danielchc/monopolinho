e89b172218b32bd7f8957a79ce4276c641dc147e
David <davidcm62@gmail.com>
2019-10-17 20:14:00 +0200

    Commit

--- src/main/java/monopolinho/obxetos/Dados.java
+++ src/main/java/monopolinho/obxetos/Dados.java
@@ -1,50 +1,87 @@
 package monopolinho.obxetos;
 import java.util.Random;
 
+
+/**
+ * @author Daniel Chenel
+ * @author David Carracedo
+ */
+
+
 public class Dados {
     private int dado1;
     private int dado2;
 
-    //// constructor ////
+
+    /**
+     * Constructor de parametros vacios.
+     * Inicia os dados a 0
+     */
     public Dados(){
         this.dado1=0;
         this.dado2=0;
     }
 
-    ////// Métodos //////
-    /*
-        Este metodo xera dous valores aleatorios para os dados.
+
+    /**
+     * Este metodo permite lanzar os dados.
+     * Xera dous valores entre 1 e 6 para cada dado.
      */
     public void lanzarDados(){
         this.dado1=new Random().nextInt(6)+1;
         this.dado2=new Random().nextInt(6)+1;
     }
 
-    /*
-        Este metodo devolve o valor dos datos sumado.
+
+    /**
+     * @return valor dos dous dados sumados
      */
     public int valorLanzar(){
         if((this.dado1==0) || (this.dado2==0))System.err.println("Non se tiraron os dados");
         return (this.dado1+this.dado2);
     }
 
-    /*
-        Este metodo permite saber se sairon dobles.
+
+    /**
+     * Permite saber se sairon dobles nos dados.
+     * @return verdadeiro se sairon dobles, falseo se non
      */
     public boolean sonDobles(){
         return (this.dado1==this.dado2);
     }
 
 
-    ////////// getter //////////////////
-
+    /**
+     * @return valor do dado 1
+     */
     public int getDado1(){
         if((this.dado1==0) || (this.dado2==0))System.err.println("Non se tiraron os dados");
         return dado1;
     }
+
+
+    /**
+     * @return valor do dado 2
+     */
     public int getDado2(){
         if((this.dado1==0) || (this.dado2==0))System.err.println("Non se tiraron os dados");
         return dado2;
     }
 
+
+    /**
+     * Permite establecer o valor do dado 1
+     * @param dado1 valor do dado1
+     */
+    public void setDado1(int dado1) {
+        this.dado1 = dado1;
+    }
+
+    /**
+     * Permite establecer o valor do dado 1
+     * @param dado2 valor do dado1
+     */
+    public void setDado2(int dado2) {
+        this.dado2 = dado2;
+    }
 }
