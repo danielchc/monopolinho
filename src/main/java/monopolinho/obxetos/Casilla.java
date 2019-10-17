a6eb439cfcc47b14d67409c32292622436ff3269
Dani <danielchenelcea@gmail.com>
2019-10-17 20:00:20 +0200

    Commit

--- src/main/java/monopolinho/obxetos/Casilla.java
+++ src/main/java/monopolinho/obxetos/Casilla.java
@@ -30,8 +30,11 @@ public class Casilla {
     private boolean estaHipotecada;
 
 
-    /////////CONSTRUCTORES/////////////////
-
+    /**
+     * Crea unha nova instancia de casilla
+     * @param nome Nome da casilla
+     * @param tipoCasilla Tipo de casilla
+     */
     public Casilla(String nome,TipoCasilla tipoCasilla){
         if(nome==null || tipoCasilla==null){
             System.err.println("Error creando a casilla");
@@ -69,32 +72,44 @@ public class Casilla {
         this.tipoCasilla=tipoCasilla;
     }
 
-    public Casilla(String nome,Grupo grupo,TipoCasilla tipoCasilla){
-        this(nome,tipoCasilla);
+    /**
+     * Crea unha nova instancia de SOLAR
+     * @param nome Nome do SOLAR
+     * @param grupo Grupo do solar
+     */
+    public Casilla(String nome,Grupo grupo){
+        this(nome,TipoCasilla.SOLAR);
         this.grupo=grupo;
         this.grupo.engadirSolar(this);
         this.colorCasilla=this.getGrupo().getColor();
     }
-    //
+
+    /**
+     * Crea unha nova casilla de IMPOSTO
+     * @param nome Nome da casilla
+     * @param tipoCasilla Tipo de Casilla(preterminado IMPOSTO neste constructor)
+     * @param imposto Imposto a pagar o pasar pola casilla
+     */
     public Casilla(String nome,TipoCasilla tipoCasilla,float imposto){
         this(nome,tipoCasilla);
         if (this.getTipoCasilla()==TipoCasilla.IMPOSTO)this.imposto=imposto;
     }
 
-
-    ////////METODOS/////////////
-    /*
-        Este metodo engade un avatar á casilla.
+    /**
+     * Engade un avatar a lista de avatares que se atopan nunha casilla
+     * NON CHAMAR ESTA FUNCIÓN FORA DE XOGADOR
+     * @param a Avatar a colocar
      */
-
     public void engadirAvatar(Avatar a){
         if(!avatares.contains(a)){
             avatares.add(a);
         }
     }
 
-    /*
-        Este método elimina un avatar da casilla.
+    /**
+     * Elimina un avatar a lista de avatares que se atopan nunha casilla
+     * NON CHAMAR ESTA FUNCIÓN FORA DE XOGADOR
+     * @param a Avatar a eliminar da casilla
      */
     public void eliminarAvatar(Avatar a){
         if(avatares.contains(a)){
@@ -102,8 +117,8 @@ public class Casilla {
         }
     }
 
-    /*
-        Este metodo permite representar aos avatares nunha casilla
+    /**
+     * @return Devolve as liñas que representan unha casilla
      */
     public String[] getRepresentacion(){
         String avataresCasilla="";
@@ -118,21 +133,49 @@ public class Casilla {
         };
     }
 
-    /*
-        Este metodo permite saber se unha casilla se pode comprar ou non.
+
+    /**
+     * @return Este metodo permite saber se unha casilla se pode comprar ou non.
      */
     public boolean podeseComprar(){
         return (this.tipoCasilla== Casilla.TipoCasilla.SOLAR || this.tipoCasilla == Casilla.TipoCasilla.SERVICIO || this.tipoCasilla== Casilla.TipoCasilla.TRANSPORTE);
     }
 
+    /**
+     * @return Devolve o valor dunha casilla
+     */
     public float getValor() {
         if(this.grupo!=null)return (float) Math.ceil((this.grupo.getValor()/this.grupo.getSolares().size()));
         else return valorServicio;
     }
+
+    /**
+     * Establece o valor dun servicio se a casilla é un servicio
+     * @param valorServicio
+     */
+    public void setValorServicio(float valorServicio) {
+        if(this.getTipoCasilla()==TipoCasilla.SERVICIO)this.valorServicio = valorServicio;
+    }
+
+    /**
+     * @return Devolve o valor do imposto da casilla actual
+     */
     public float getImposto() {
         if(this.getTipoCasilla()==TipoCasilla.IMPOSTO)return imposto;
         else return -1;
     }
+
+    /**
+     * Establece o imposto a pagar por caer nesta casilla, se a casilla é un servizo
+     * @param imposto
+     */
+    public void setImposto(float imposto) {
+        if(this.getTipoCasilla()==TipoCasilla.IMPOSTO)this.imposto = imposto;
+    }
+
+    /**
+     * @return Devolve o prezo a pagar pola hipoteca
+     */
     public float getHipoteca(){
         if(podeseComprar()){
             return getValor()*Valor.FACTOR_HIPOTECA;
@@ -140,47 +183,69 @@ public class Casilla {
         return -1;
     }
 
-    ///////// Getters e Setters //////////
-
-
-    public boolean getEstaHipotecada() {
-        return estaHipotecada;
-    }
-
-    public void setEstaHipotecada(boolean estaHipotecada) {
-        this.estaHipotecada = estaHipotecada;
-    }
-
-    public TipoCasilla getTipoCasilla() {
-        return tipoCasilla;
-    }
-
+    /**
+     * @return Devolve o valor a pagar polo alquiler da casilla
+     */
     public float getAlquiler() {
         return getValor()*Valor.FACTOR_ALQUILER;
     }
 
+    /**
+     * @return Devolve se unha casilla está hipotecada
+     */
+    public boolean getEstaHipotecada() {
+        return estaHipotecada;
+    }
+
+    /**
+     * Establece se unha casilla está hipotecada
+     * @param estaHipotecada
+     */
+    public void setEstaHipotecada(boolean estaHipotecada) {
+        this.estaHipotecada = estaHipotecada;
+    }
+
+    /**
+     * @return Devolve o tipo de casilla
+     */
+    public TipoCasilla getTipoCasilla() {
+        return tipoCasilla;
+    }
+
+    /**
+     * Establece o tipo de casilla
+     * @param tipoCasilla
+     */
+    public void setTipoCasilla(TipoCasilla tipoCasilla) {
+        this.tipoCasilla = tipoCasilla;
+    }
+
+    /**
+     * @return Devolve o nome da casilla
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Establece o nome dunha casilla
+     * @param nome
+     */
+    public void setNome(String nome) {
+        if(nome!=null)this.nome = nome;
+    }
+
+    /**
+     * @return Devolve o dono da casilla
+     */
     public Xogador getDono() {
         return dono;
     }
 
-
-
-    public void setImposto(float imposto) {
-        if(this.getTipoCasilla()==TipoCasilla.IMPOSTO)this.imposto = imposto;
-    }
-
-    public Grupo getGrupo() {
-        return grupo;
-    }
-
-    public int getPosicionIndex() {
-        return posicion;
-    }
-
+    /**
+     * Establece o xogador dono de esta casilla
+     * @param dono Xogador dono de esta casilla
+     */
     public void setDono(Xogador dono) {
         if(dono!=null){
             if(this.dono!=null)
@@ -190,22 +255,83 @@ public class Casilla {
         }
     }
 
-    public void setPosicion(int posicion) {
+    /**
+     * @return Devolve o grupo que pertenece esta casilla
+     */
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    /**
+     * Establece o grupo dunha casilla, solo para solares
+     * @param grupo
+     */
+    public void setGrupo(Grupo grupo) {
+        if(grupo!=null)this.grupo = grupo;
+    }
+
+    /**
+     * @return Devolve a posición da casilla no taboeiro, do 0 o 39
+     */
+    public int getPosicionIndex() {
+        return posicion;
+    }
+
+    /**
+     * Establece o indice no taboeiro da casilla
+     * @param posicion
+     */
+    public void setPosicionIndex(int posicion) {
         this.posicion = posicion;
     }
 
+    /**
+     * @return Devolve o valor dunha casilla
+     */
+    public Valor.ReprColor getColorCasilla() {
+        return colorCasilla;
+    }
+
+    /**
+     * @param colorCasilla Establece a cor dunha casilla
+     */
+    public void setColorCasilla(Valor.ReprColor colorCasilla) {
+        this.colorCasilla = colorCasilla;
+    }
+
+    /**
+     * @return Devolve o taboeiro no que se atopa a casilla
+     */
     public Taboeiro getTaboeiro() {
         return taboeiro;
     }
 
+    /**
+     * Establece o taboeiro no que se atopa a casilla
+     * @param taboeiro
+     */
     public void setTaboeiro(Taboeiro taboeiro) {
         if(taboeiro!=null)this.taboeiro = taboeiro;
     }
 
+    /**
+     * @return Devolve os avatares que se atopan na casilla
+     */
+    public ArrayList<Avatar> getAvatares() {
+        return avatares;
+    }
 
+    /**
+     * Establece os avatares dunha casilla
+     * @param avatares
+     */
+    public void setAvatares(ArrayList<Avatar> avatares) {
+        if(avatares!=null)this.avatares = avatares;
+    }
 
-    /////////////////////////////////////overrides//////////////////////////////
-
+    /**
+     * @return Devolve a información dunha casilla
+     */
     @Override
     public String toString(){
         String xogadores="";
@@ -224,33 +350,36 @@ public class Casilla {
                         "Bote: "+this.taboeiro.getBote()+"\n\t" +
                         "Xogadores:["+xogadores+"\n\t]\n}";
                 break;
-
             case CARCEL:
                 texto="{\n\t" +
                         "Tipo: "+this.tipoCasilla+",\n\t" +
                         "Salir:"+ Valor.SAIR_CARCERE+",\n\t" +
-                        "Xogadores:["+xogadoresCarcel+"\n\t]\n}"; //IMPLEMENTAR ESTO
+                        "Xogadores:["+xogadoresCarcel+"\n\t]" +
+                        "\n}";
                 break;
             case IMPOSTO:
                 texto="{\n\t" +
-                        "Tipo: "+this.tipoCasilla+",\n\t" +
-                        "Pagar:"+ getImposto() + "\n}";
+                            "Tipo: "+this.tipoCasilla+",\n\t" +
+                            "Pagar:"+ getImposto() +
+                        "\n}";
                 break;
             case SERVICIO:
             case TRANSPORTE:
-                texto="{"+"\n\tNome: "+this.nome+((this.estaHipotecada)?"(Hipotecada)":"")+"\n\t" +
-                        "Tipo: "+this.tipoCasilla+"\n\t" +
-                        "Precio: "+this.getValor()+
-                        ((!this.dono.getNome().equals("Banca"))?"\n\tPropietario: "+this.dono.getNome():"") +
+                texto="{"+
+                            "\n\tNome: "+this.nome+((this.estaHipotecada)?"(Hipotecada)":"")+"\n\t" +
+                            "Tipo: "+this.tipoCasilla+"\n\t" +
+                            "Precio: "+this.getValor()+
+                            ((!this.dono.getNome().equals("Banca"))?"\n\tPropietario: "+this.dono.getNome():"") +
                         "\n}";
                 break;
             default:
-                texto="{"+"\n\tNome: "+this.nome+((this.estaHipotecada)?"(Hipotecada)":"")+"\n\t" +
-                        "Tipo: "+this.tipoCasilla+"\n\t" +
-                        "Grupo: "+this.getGrupo().getNome()+"\n\t" +
-                        "Valor: "+this.getValor()+"\n\t" +
-                        "Alquiler: "+this.getAlquiler()+
-                        ((!this.dono.getNome().equals("Banca"))?"\n\tPropietario: "+this.dono.getNome():"") +
+                texto="{"+
+                            "\n\tNome: "+this.nome+((this.estaHipotecada)?"(Hipotecada)":"")+"\n\t" +
+                            "Tipo: "+this.tipoCasilla+"\n\t" +
+                            "Grupo: "+this.getGrupo().getNome()+"\n\t" +
+                            "Valor: "+this.getValor()+"\n\t" +
+                            "Alquiler: "+this.getAlquiler()+
+                            ((!this.dono.getNome().equals("Banca"))?"\n\tPropietario: "+this.dono.getNome():"") +
                         "\n}";
                 break;
 
@@ -258,6 +387,11 @@ public class Casilla {
         return texto;
     }
 
+    /**
+     * Compara se os obxetos son iguais
+     * @param obj Obxeto a comparar
+     * @return Son iguais os obxectos
+     */
     @Override
     public boolean equals(Object obj){
         if(obj instanceof Casilla){
